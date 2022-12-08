d6c19c4d14c427db172e0fc059e1cad9c96007a7
Jolu <joluquispe2@gmail.com>
2022-12-08 13:40:40 -0500

    aqui se instalo laravel al proyecto

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>curso de git y composer</title>
+</head>
+<body>
+    <p>es mi primer proyecto</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia maiores blanditiis praesentium perferendis veritatis. Incidunt, excepturi? Error, tenetur quas molestiae harum laborum accusantium velit praesentium laboriosam aut. Aliquid, omnis eum.</p>
+
+</body>
+</html>

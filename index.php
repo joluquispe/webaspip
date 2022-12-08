7cb4674d4c82a6a9abf299c35a6bc1a9ad81dc8b
Jolu <joluquispe2@gmail.com>
2022-12-08 13:47:11 -0500

    se modifico el index

--- index.php
+++ index.php
@@ -11,5 +11,7 @@
 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia maiores blanditiis praesentium perferendis veritatis. Incidunt, excepturi? Error, tenetur quas molestiae harum laborum accusantium velit praesentium laboriosam aut. Aliquid, omnis eum.</p>
 
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint nisi iusto, culpa quo, est nemo ex illum reprehenderit sapiente officia distinctio id itaque dignissimos pariatur fugit autem cupiditate rerum magnam! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil sequi sed neque, beatae aperiam ducimus fuga officiis cum eveniet architecto, minus eaque, incidunt eius quae est consequatur animi maiores atque.</p>
+
 </body>
 </html>
